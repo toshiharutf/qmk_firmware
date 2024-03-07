ebede09ad2f9e4e3ac32bb0bb88113aad2e48f49
toshi <rttf06@gmail.com>
2024-03-07 14:41:56 +0400

    added backslash

--- keyboards/crkbd/keymaps/rttf/keymap.c
+++ keyboards/crkbd/keymaps/rttf/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [L_2] = LAYOUT_split_3x6_3(
     //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        _______, XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX, XXXXXXX,                    XXXXXXX,  KC_AT, KC_HASH, KC_DLR, KC_TILD, KC_BSPC,
+        _______, XXXXXXX, XXXXXXX, XXXXXXX,  KC_BACKSLASH, XXXXXXX,                    XXXXXXX,  KC_AT, KC_HASH, KC_DLR, KC_TILD, KC_BSPC,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
         _______, KC_HOME,  KC_UP,  KC_END,  KC_PGUP,  XXXXXXX,                    LCTL(KC_SLSH), KC_COLN, KC_SEMICOLON, KC_EXLM, KC_GRAVE, KC_DEL,
     //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
